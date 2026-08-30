25337d281a0428b60eb7d09160fc6404d9178aef
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2026-08-30 08:12:40 +0700

    high score

--- snake_game/main.py
+++ snake_game/main.py
@@ -14,13 +14,13 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-game_is_on = True
-
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
+
+game_is_on = True
 
 while game_is_on:
     screen.update()
@@ -35,13 +35,13 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
---
+++ snake_game/test.py
@@ -0,0 +1,4 @@
+a = 3
+print(a)
+a = input("What do you want a to do equal?:")
+print(a)
